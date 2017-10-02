ac7eb4ec93abac410eb4e35b2d78fc3d3a856e5b
Conrad Crates <conrad-crates@hotmail.co.uk>
2017-10-02 16:53:30 +0100

    Defined main layout of app

--- app/src/main/java/com/conradcrates/pulselivetechnicalchallenge/MainActivity.java
+++ app/src/main/java/com/conradcrates/pulselivetechnicalchallenge/MainActivity.java
@@ -2,12 +2,20 @@ package com.conradcrates.pulselivetechnicalchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView articles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        articles = (RecyclerView)findViewById(R.id.list_articles);
+        articles.setLayoutManager(new LinearLayoutManager(this));
+        articles.setAdapter(null);
     }
 }
