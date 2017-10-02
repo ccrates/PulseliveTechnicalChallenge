cb9a4ccf3fe66b236f4d10c124c6dfa0664df1cb
Conrad Crates <conrad-crates@hotmail.co.uk>
2017-10-02 16:45:59 +0100

    Created article class to store data

---
+++ app/src/main/java/com/conradcrates/pulselivetechnicalchallenge/data/Article.java
@@ -0,0 +1,54 @@
+package com.conradcrates.pulselivetechnicalchallenge.data;
+
+import java.util.Date;
+
+/**
+ * Created by Conrad on 02/10/2017.
+ */
+
+public class Article {
+
+    private int id;
+    private String title, subtitle, body;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
